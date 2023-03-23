711fe43782495dc90a69c0aa8990bdd57fb7f36c
Parisienne <96126061+iParisienne@users.noreply.github.com>
2023-03-23 10:17:33 +0100

    add for loop to create append 16x divs

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 const body = document.body; //save body in variable
 const wrapper = document.createElement('div'); //create wrapper div
-wrapper.classList.add('wrapper');
-body.appendChild(wrapper); //append wrapper div to body element
-const divs = document.createElement('div'); //create divs
 const amountOfDivs = 16; //define how many divs should be created
 
-divs.classList.add('square'); //add class square to divs
+wrapper.classList.add('wrapper'); //add class wrapper to wrapper
+body.appendChild(wrapper); //append wrapper div to body element
 
 for(let i = 0; i < amountOfDivs; i++){
-
+    const div = document.createElement('div'); //create div
+    div.classList.add('square'); //add class square to div
+    wrapper.appendChild(div);  //append div to wrapper
 }
 
