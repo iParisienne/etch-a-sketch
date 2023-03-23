7f1917eec46f5016ff9067d52f85df3bfbabd190
Parisienne <96126061+iParisienne@users.noreply.github.com>
2023-03-23 11:01:58 +0100

    add wrapper for grid element

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
 const body = document.body; 
+const wrapper = document.createElement('div');
 const grid = document.createElement('div'); 
 const amountOfDivsX = 16; //define how many divs should be created on x-axis
 const amountOfDivsY = 16; //y-axis
 
+wrapper.classList.add('wrapper');
+body.appendChild(wrapper);
 grid.classList.add('grid'); 
-body.appendChild(grid); 
+wrapper.appendChild(grid); //put grid inside of wrapper so that it's own element for the border property
 
 for(let i = 0; i < amountOfDivsX; i++){ // append ${amountOfDivsX} to grid div
     const divX = document.createElement('div'); 
