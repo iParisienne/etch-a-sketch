62b2b7968eaaa1f0f7b9477a268f8e46640b7bf9
Parisienne <96126061+iParisienne@users.noreply.github.com>
2023-03-23 11:47:55 +0100

    Add class parentSquare and fixed size for grid

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 const body = document.body; 
 const wrapper = document.createElement('div');
 const grid = document.createElement('div'); 
-const amountOfDivsX = 16; //define how many divs should be created on x-axis
-const amountOfDivsY = 16; //y-axis
+let amountOfDivsX = 16; //define how many divs should be created on x-axis
+let amountOfDivsY = 16; //y-axis
 
 wrapper.classList.add('wrapper');
 body.appendChild(wrapper);
@@ -11,6 +11,7 @@ wrapper.appendChild(grid); //put grid inside of wrapper so that it's own element
 
 for(let i = 0; i < amountOfDivsX; i++){ // append ${amountOfDivsX} to grid div
     const divX = document.createElement('div'); 
+    divX.classList.add('parentSquare');
     for(let j = 0; j < amountOfDivsY; j++){ // append ${amountOfDivsY} to each divX div
         const divY = document.createElement('div');
         divY.classList.add('square');
