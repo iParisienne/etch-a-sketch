3bc6b7af97f1bafd33ac0c39490102902cddabbd
Parisienne <96126061+iParisienne@users.noreply.github.com>
2023-03-29 12:11:49 +0200

    Add adjustable grid javascript, put existing code in functions

--- js/script.js
+++ js/script.js
@@ -11,15 +11,23 @@ main.appendChild(wrapper);
 grid.classList.add('grid'); 
 wrapper.appendChild(grid); //put grid inside of wrapper so that it's own element for the border property
 
-for(let i = 0; i < amountOfDivsX; i++){ // append ${amountOfDivsX} to grid div
-    const divX = document.createElement('div'); 
-    divX.classList.add('parentSquare');
-    for(let j = 0; j < amountOfDivsY; j++){ // append ${amountOfDivsY} to each divX div
-        const divY = document.createElement('div');
-        divY.classList.add('square');
-        divX.appendChild(divY);
+function createGrid(x, y){
+    for(let i = 0; i < x; i++){ // append ${amountOfDivsX} to grid div
+        const divX = document.createElement('div'); 
+        divX.classList.add('parentSquare');
+        for(let j = 0; j < y; j++){ // append ${amountOfDivsY} to each divX div
+            const divY = document.createElement('div');
+            divY.classList.add('square');
+            divX.appendChild(divY);
+        }
+        grid.appendChild(divX); 
     }
-    grid.appendChild(divX); 
+}
+
+function resetGrid(){
+    while(grid.firstChild){
+        grid.removeChild(grid.lastChild);
+    }
 }
 
 function mouseOverBlack(e){
@@ -36,25 +44,31 @@ function mouseOutOpacity(){
     this.style.opacity = '0.5';
 }
 
-const divs = Array.from(document.querySelectorAll('.square'));
-divs.forEach(
-    div => {
-        div.addEventListener('mouseover', mouseOverBlack);
-    }
-);
-
-stylebar.addEventListener('mouseover', mouseOverOpacity);
-stylebar.addEventListener('mouseout', mouseOutOpacity);
-
-if(gridsizeInput.value == ''){
-    gridsizeInput.value = amountOfDivsX;
+function addClassSetBlack(){
+    const divs = Array.from(document.querySelectorAll('.square'));
+    divs.forEach(
+        div => {
+            div.addEventListener('mouseover', mouseOverBlack);
+        }
+    )
 }
 
 function onGridSizeInputChange(){
     amountOfDivsX = gridsizeInput.value;
     amountOfDivsY = gridsizeInput.value;
+    resetGrid();
+    createGrid(amountOfDivsX, amountOfDivsY);
+    addClassSetBlack();
 }
 
+if(gridsizeInput.value == ''){
+    gridsizeInput.value = amountOfDivsX;
+    createGrid(amountOfDivsX, amountOfDivsY);
+    addClassSetBlack();
+}
+
+stylebar.addEventListener('mouseover', mouseOverOpacity);
+stylebar.addEventListener('mouseout', mouseOutOpacity);
 gridsizeInput.addEventListener('change', onGridSizeInputChange);
 
 function control(){
