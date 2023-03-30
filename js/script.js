e8596e575d07ee0ed6c21ce641ee9dee4c0d635e
Parisienne <96126061+iParisienne@users.noreply.github.com>
2023-03-30 09:34:50 +0200

    add js and buttons for random colors and fadeblack, adjustments to css, finalize project

--- js/script.js
+++ js/script.js
@@ -4,8 +4,16 @@ const grid = document.createElement('div');
 const stylebar = document.querySelector('.stylebar');
 const gridsizeInput = document.querySelector('#gridsize-input');
 const resetGridBtn = document.querySelector('.resetGrid-btn');
+const colorpickerInput = document.querySelector('#colorpicker-input');
+const randomColorBtn = document.querySelector('.randomCol-btn');
+const fadeBlackBtn = document.querySelector('.fadeBlack-btn')
 let amountOfDivsX = 16; //define how many divs should be created on x-axis
 let amountOfDivsY = 16; //y-axis
+let gridCreatedBool = false;
+let fixedColorBool = true;
+let blackFadeBool = false;
+let randomColorBool = false;
+let A = 0.1;
 
 wrapper.classList.add('wrapper');
 main.appendChild(wrapper);
@@ -19,9 +27,10 @@ function defaultValuesGrid(){
 }
 
 function defaultOperationGrid(){
-    resetGrid();
+    if(gridCreatedBool)
+        resetGrid();
     createGrid(amountOfDivsX, amountOfDivsY);
-    addClassSetBlack();
+    setColor();
 }
 
 function createGrid(x, y){
@@ -35,12 +44,11 @@ function createGrid(x, y){
         }
         grid.appendChild(divX); 
     }
+    gridCreatedBool = true;
 }
 
 function resetGrid(){ 
-    while(grid.firstChild){
-        grid.removeChild(grid.lastChild);
-    }
+    grid.innerHTML = '';
 }
 
 function resetGridBtnFunc(){ 
@@ -48,10 +56,58 @@ function resetGridBtnFunc(){
     defaultOperationGrid();
 }
 
-function mouseOverBlack(e){ 
-    if(e.buttons == 1){
-        this.classList.add('setBlack');
+function randomColors(){
+    let randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+    return randomColor;
+}
+
+function fadeIntoBlack(){
+    /* Random generated hue and saturation
+    let H = Math.floor(Math.random()*360).toString();
+    let S = Math.floor(Math.random()*100).toString();
+    */
+    A += 0.1;
+    if(A > 1)
+        A = 0.1;
+    let fadeBlackColor = `hsla(0,0%,0%,${A})`;
+    console.log(A);
+    return fadeBlackColor;
+}
+
+function activateRandomColors(){
+    if(fixedColorBool || blackFadeBool){
+        fixedColorBool = false;
+        blackFadeBool = false;
+        randomColorBool = true;
     }
+}
+
+function activateFadeIntoBlack(){
+    if(fixedColorBool || randomColorBool){
+        fixedColorBool = false;
+        randomColorBool = false;
+        blackFadeBool = true;
+    }
+}
+
+function mouseOverPaint(e){ 
+    if(e.buttons == 1){
+        if(fixedColorBool)
+            this.style.background = colorpickerInput.value;
+        else if(randomColorBool)
+            this.style.background = randomColors();
+        else if(blackFadeBool)
+            this.style.background = fadeIntoBlack();
+    }
+}
+
+function mouseClickPaint(){
+    if(fixedColorBool)
+        this.style.background = colorpickerInput.value;
+    else if(randomColorBool)
+        this.style.background = randomColors();
+    else if(blackFadeBool)
+        this.style.background = fadeIntoBlack();
 }
 
 function mouseOverOpacity(){
@@ -62,11 +118,12 @@ function mouseOutOpacity(){
     this.style.opacity = '0.5';
 }
 
-function addClassSetBlack(){
+function setColor(){
     const divs = Array.from(document.querySelectorAll('.square'));
     divs.forEach(
         div => {
-            div.addEventListener('mouseover', mouseOverBlack);
+            div.addEventListener('mouseover', mouseOverPaint);
+            div.addEventListener('click', mouseClickPaint);
         }
     )
 }
@@ -81,19 +138,15 @@ function onGridSizeInputChange(){
     defaultOperationGrid();
 }
 
-if(gridsizeInput.value == ''){
-    gridsizeInput.value = amountOfDivsX;
-    createGrid(amountOfDivsX, amountOfDivsY);
-    addClassSetBlack();
-}
+defaultValuesGrid();
+defaultOperationGrid();
 
 stylebar.addEventListener('mouseover', mouseOverOpacity);
 stylebar.addEventListener('mouseout', mouseOutOpacity);
 gridsizeInput.addEventListener('change', onGridSizeInputChange);
 resetGridBtn.addEventListener('click', resetGridBtnFunc);
-
-function control(){
-    console.log(amountOfDivsX);
-    console.log(amountOfDivsY);
-}
-
+randomColorBtn.addEventListener('click', activateRandomColors);
+fadeBlackBtn.addEventListener('click', activateFadeIntoBlack);
+colorpickerInput.addEventListener('input', function() {  
+    fixedColorBool = true;
+});
